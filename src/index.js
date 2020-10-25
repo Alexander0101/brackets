921de60c5d513eea9931176f7ed4f765a51095db
Alex <ovcharenko@octarine.com.ua>
2020-10-25 21:20:52 +0200

    feat: add brackets

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  arr = Object.fromEntries(bracketsConfig);
+  let stack = [];
+  for (let i = 0; i < str.length; i++) {
+    let el = str[i];
+    if (arr[el]) {
+      stack.push(arr[el]);
+    } else {
+      if (el !== stack.pop()) {
+        return true;
+      }
+    }
+  }
+  return stack.length === 0;
 }
